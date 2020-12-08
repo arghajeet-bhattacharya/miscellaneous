d111384c267953eb45f9aff60ccf762b56944ec5
Arghajeet Bhattacharya <37384779+arghajeet-bhattacharya@users.noreply.github.com>
2020-12-08 11:19:37 +0530

    Update palin.js

--- palin.js
+++ palin.js
@@ -24,12 +24,3 @@ for(i=0;i<size;i++) {
 }
 
 console.log("isPalin: ", isPalin);
-
-
-Create an Angular / React component which will have a progress bar. It will increment after 10 secs. The progress bar will keep on incrementing till the width of the page is met and then it will stop incrementing.
-        
-Print the first “n” prime numbers. N is anything a user might enter.
-  Example n = 5 Output is 2,3,5,7,11
-  Example n = 8 Output is 2,3,5,7,11,13,17,19.
-
-Write an algorithm which will tell if a group of characters can be a probable palindrome string with its different combination. Example “rarecac”…. Its possible combinations can be rraacec, racecar etc. Now Racecar is the palindrome, hence return true
